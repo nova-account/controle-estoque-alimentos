8fef964efc683c62709a23f6244d13ee4f36ed27
Ana <beatriz.nova@ufrpe.br>
2025-09-17 19:19:45 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,2 +1,31 @@
 public class Funcionario {
+    private int id;
+    private String nome;
+    private String cargo;
+    private String login;
+    private String senha;
+
+    public Funcionario(int id, String nome, String cargo, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public boolean autenticar(String login, String senha) {
+        return this.login.equals(login) && this.senha.equals(senha);
+    }
+
+    // --- Getters e Setters ---
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
 }
